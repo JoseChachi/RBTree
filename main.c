e64d83f69adc9c9fbc5c8aa78be75a0aa01cc13c
Jose Chachi <jose.chachi@utec.edu.pe>
2023-11-09 13:17:40 -0500

    feat: Add support (hard-coded) for a struct thread in RBTree.

--- main.c
+++ main.c
@@ -3,32 +3,44 @@
 
 int main(){
     struct RBTree* tri;
-    tri = init_tree(tri);
-    struct RBNode* four = init_node(4);
-    struct RBNode* two = init_node(2);
-    struct RBNode* uan = init_node(1);
-    struct RBNode* ocho = init_node(8);
-    struct RBNode* nove = init_node(9);
-    struct RBNode* ten = init_node(10);
-    struct RBNode* trii = init_node(3);
+    tri = init_tree();
 
+    struct thread* hilo5 = init_thread(5, 5);
+    struct RBNode* faiv = init_node_with_thread(hilo5);
+    insert(tri, faiv);
+    struct thread* hilo4 = init_thread(4, 4);
+    struct RBNode* four = init_node_with_thread(hilo4);
     insert(tri, four);
-    print_inorder(tri);
-    insert(tri, two);
-    print_inorder(tri);
-    insert(tri,uan);
-    print_inorder(tri);
-    insert(tri, ocho);
-    print_inorder(tri);
-    insert(tri, nove);
-    print_inorder(tri);
-    insert(tri, ten);
-    print_inorder(tri);
+    struct thread* hilo3 = init_thread(3, 100);
+    struct RBNode* trii = init_node_with_thread(hilo3);
     insert(tri, trii);
     print_inorder(tri);
+    // struct RBNode* four = init_node(4);
+    // struct RBNode* two = init_node(2);
+    // struct RBNode* uan = init_node(1);
+    // struct RBNode* ocho = init_node(8);
+    // struct RBNode* nove = init_node(9);
+    // struct RBNode* ten = init_node(10);
+    // struct RBNode* trii = init_node(3);
 
-    delete(tri, ocho);
-    print_inorder(tri);
+    // insert(tri, four);
+    // print_inorder(tri);
+    // insert(tri, two);
+    // print_inorder(tri);
+    // insert(tri,uan);
+    // print_inorder(tri);
+    // insert(tri, ocho);
+    // print_inorder(tri);
+    // insert(tri, nove);
+    // print_inorder(tri);
+    // insert(tri, ten);
+    // print_inorder(tri);
+    // insert(tri, trii);
+    // print_inorder(tri);
+
+
+    // delete(tri, ocho);
+    // print_inorder(tri);
     //testing delete
     // print_node(tri, tri->root);
     // print_node(tri, tri->root->rightNode);
--- rbtree.h
+++ rbtree.h
@@ -7,8 +7,29 @@ enum COLOUR {
     RED = 1
 };
 
+struct thread{
+    int priority;
+
+    int thread_id;
+
+    int secret_code;
+};
+
+struct thread* init_thread(int thread_id, int priority){
+    struct thread* newThread;
+    
+    newThread = (struct thread*) malloc(sizeof(struct thread));
+
+    newThread->thread_id = thread_id;
+    newThread->priority = priority;
+
+    newThread->secret_code = 123456;
+
+    return newThread;
+}
+
 struct RBNode{
-    int value;
+    struct thread* value;
 
     struct RBNode* parent;
     struct RBNode* leftNode;
@@ -24,11 +45,24 @@ struct RBTree{
     int size;
 };
 
-struct RBNode* init_node(int value){
+// struct RBNode* init_node(int value){
+//     struct RBNode* node;
+//     node = (struct RBNode*) malloc(sizeof(struct RBNode));
+
+//     node->value = value;
+//     node->parent = NULL;
+//     node->leftNode = NULL;
+//     node->rightNode = NULL;
+//     node->colour = RED;
+
+//     return node;
+// }
+
+struct RBNode* init_node_with_thread(struct thread* hilo){
     struct RBNode* node;
     node = (struct RBNode*) malloc(sizeof(struct RBNode));
 
-    node->value = value;
+    node->value = hilo;
     node->parent = NULL;
     node->leftNode = NULL;
     node->rightNode = NULL;
@@ -42,7 +76,8 @@ struct RBTree* init_tree(){
     struct RBTree* tree;
     tree = (struct RBTree*) malloc(sizeof(struct RBTree));
 
-    struct RBNode* nullspace = init_node(0);
+    struct thread* threadNull = init_thread(0,0);
+    struct RBNode* nullspace = init_node_with_thread(threadNull);
     nullspace->colour = BLACK;
 
     tree->size = 0;
@@ -140,17 +175,17 @@ void recolour(struct RBTree* tree, struct RBNode* z){
 void insert(struct RBTree* tri, struct RBNode* z){
     struct RBNode* tmp = tri->root;
     struct RBNode* y = tri->nullSpace;
-
+    tri->size += 1;
     while(tmp != tri->nullSpace){
         y = tmp;
-        if(z->value < tmp->value) tmp = tmp->leftNode;
+        if(z->value->priority < tmp->value->priority) tmp = tmp->leftNode;
         else tmp = tmp->rightNode;
     }
 
     z->parent = y;
 
     if(y == tri->nullSpace) tri->root = z;
-    else if(z->value < y->value) y->leftNode = z;
+    else if(z->value->priority < y->value->priority) y->leftNode = z;
     else y->rightNode = z;
 
     z->leftNode = tri->nullSpace;
@@ -245,6 +280,9 @@ void delete(struct RBTree* tri, struct RBNode* z){
     struct RBNode* y = z;
     struct RBNode* x;
     enum COLOUR originalColour = y->colour;
+
+    tri->size -= 1;
+
     if(z->leftNode == tri->nullSpace){
         x = z->rightNode;
         transplant(tri, z, z->rightNode);
@@ -277,8 +315,8 @@ void delete(struct RBTree* tri, struct RBNode* z){
 void print_inorder_helper(struct RBTree* tri, struct RBNode* node){
     if(node != tri->nullSpace){
         print_inorder_helper(tri, node->leftNode);
-        if(node->colour) printf("%d|R\t", node->value);
-        else printf("%d|B\t", node->value);
+        if(node->colour) printf("%d|R\t", node->value->priority);
+        else printf("%d|B\t", node->value->priority);
         print_inorder_helper(tri, node->rightNode);
     }
 }
@@ -290,13 +328,13 @@ void print_inorder(struct RBTree* tri){
 
 void print_node(struct RBTree* tri, struct RBNode* node){
     if(node != tri->nullSpace){
-        if(node->colour) printf("---Printing info of node %d|R---\n", node->value);
-        else printf("---Print info of node %d|B---\n", node->value);
-        if(node->leftNode != tri->nullSpace) printf("My left is %d\n", node->leftNode->value);
+        if(node->colour) printf("---Printing info of node %d|R---\n", node->value->priority);
+        else printf("---Print info of node %d|B---\n", node->value->priority);
+        if(node->leftNode != tri->nullSpace) printf("My left is %d\n", node->leftNode->value->priority);
         else printf("I have no left\n");
-        if(node->rightNode != tri->nullSpace) printf("My right is %d\n", node->rightNode->value);
+        if(node->rightNode != tri->nullSpace) printf("My right is %d\n", node->rightNode->value->priority);
         else printf("I have no right\n");
-        if(node->parent != tri->nullSpace) printf("My parent is %d\n", node->parent->value);
+        if(node->parent != tri->nullSpace) printf("My parent is %d\n", node->parent->value->priority);
         else printf("I have no parent\n");
         
     }
